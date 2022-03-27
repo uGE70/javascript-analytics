4e3714c084b37c90422982e8911165f9d5b4e832
Christophe Lance <3645531+uGE70@users.noreply.github.com>
2022-03-27 18:17:03 +0200

    Update get_email.js

--- get_email.js
+++ get_email.js
@@ -1,10 +1,10 @@
 // Get email from #Email field and send it to Google Analytics
 window.addEventListener("DOMContentLoaded", function () {
-	if (typeof(gtag) === "function") { // Is GA loaded ?
-	const ga_email = document.getElementById("Email");
-  	ga_email.addEventListener("blur", function () {
-    //console.log("Email " + ga_email.value);
-    gtag('event', 'track_form', { 'event_category': 'engagement', 'email_address':  ga_email.value });
+  if (typeof(gtag) === "function") { // Is GA loaded ?
+    const ga_email = document.getElementById("Email");
+    ga_email.addEventListener("blur", function () {
+      //console.log("Email " + ga_email.value);
+      gtag('event', 'track_form', { 'event_category': 'engagement', 'email_address':  ga_email.value });
     });
-   }
-   });
+  }
+});
