2dd2ef619f6e6dcd03371064db5ffc21c30f9c51
Christophe Lance <3645531+uGE70@users.noreply.github.com>
2022-05-20 15:56:26 +0200

    Create click-tracking.js

---
+++ click-tracking.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function () {
+  if (typeof(gtag) === "function") { // Is GA loaded ?
+  const ga_buttons = document.querySelectorAll('.button-tel'); // Check buttons
+  ga_buttons.forEach(query_button => {
+    query_button.addEventListener("click", () => {
+      gtag('event', 'Click', {
+        'event_category': 'button',
+        'event_action': 'Click',
+        'event_label': 'button-tel',
+      });
+    });
+  });
+  };
+});
