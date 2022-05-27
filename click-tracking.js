adf3d165acc3ed40b6fe03a3e38815f2f1434fc5
Christophe Lance <3645531+uGE70@users.noreply.github.com>
2022-05-27 18:47:07 +0200

    Update click-tracking.js

--- click-tracking.js
+++ click-tracking.js
@@ -3,12 +3,14 @@ window.addEventListener("DOMContentLoaded", () => {
     const ga_buttons = document.querySelectorAll('.button-track'); // Check buttons
     ga_buttons.forEach(query_button => {
       query_button.addEventListener("click", () => {
-        const gcode = query_button.getAttribute('data-track');
-        gtag('event', gcode, {
-          'event_category': 'button',
-          'event_action': 'Click',
-          'event_label': 'button',
-        });
+        if (query_button.hasAttribute('data-track')) {
+          const gcode = query_button.getAttribute('data-track');
+          gtag('event', gcode, {
+            'event_category': 'button',
+            'event_action': 'Click',
+            'event_label': 'button',
+          });
+        }
       });
     });
   };
